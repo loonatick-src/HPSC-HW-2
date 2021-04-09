48c13636ca35b24f60ab064e32cffd34ae59ec6f
Chaitanya Kumar <chaitanyak28771@gmail.com>
2021-04-09 20:49:03 +0530

    MPI matrix multiplication baseline working

--- src/impl_mpi.c
+++ src/impl_mpi.c
@@ -1,315 +1,133 @@
 #include "dbg.h"
 #include "impl_mpi.h"
-#include <inttypes.h>
+
 #include <mpi.h>
-#include <stdint.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-#define NOT_IMPLEMENTED_ERROR 30
-
+#define NOT_IMPLEMENTED 30
 
 int
 read_matrices(double *M_1, double *M_2,
-        FILE *m1_file, FILE *m2_file, int width, 
+        FILE *m1_file, FILE *m2_file, int width,
         int proc_rank, int num_procs)
 {
-   /* reads matrices from the files into the malloc'd memory */
-    const int matrix_size = width * width;
-    int mpi_init_flag;
-    int mpi_err = MPI_Initialized(&mpi_init_flag);
-    check(!mpi_err, "Call to `MPI_Initialized` returned with error");
-    debug("MPI_Initialized crossed");
-    check(mpi_init_flag, "No MPI environment was found to be initialized");
-
-    if (proc_rank == 0)
-    {
-    check_mem(M_1);
-    check_mem(M_2);
-    check(m1_file, "Unopened file has been passed");
-    check(m2_file, "Unopened file has been passed");
-
-    for (int i = 0; i < matrix_size; i++)
-    {
-        int fscanf_ret = fscanf(m1_file, "%lf", &M_1[i]);
-        check(fscanf_ret != EOF, "Unexpected EOF, left matrix. Scanned %d elements, promised %d elements", i, matrix_size);
-        check(fscanf_ret != 0, "Failed to scan anything after scanning %d elements of the left matrix", i);
-        fscanf_ret = fscanf(m2_file, "%lf", &M_2[i]);
-        check(fscanf_ret != EOF, "Unexpected EOF, right matrix. Scanned %d elements, promised %d elements", i, matrix_size);
-        check(fscanf_ret != 0, "Failed to scan anything after scanning %d elements of the right matrix", i);
-    }
-    }
-
-    return EXIT_SUCCESS;
-error:
-    // let caller handle error
-    return EXIT_FAILURE;
+    return NOT_IMPLEMENTED;
 }
 
-
 int
-matMulSquare_transpose_mpi(const double *M_1, double *M_2, double *P,
-        int width, int proc_rank, int num_procs)
+matMulSquare_baseline_mpi(const double *M_1, double *M_2,
+        double *P, int width,
+        int proc_rank, int num_procs)
 {
-    const int num_rows_per_proc = width / num_procs;
-    check(num_rows_per_proc > 0, 
-            "Poorly balanced problem;\
-            width of matrices: %d, number of processes: %d",
-            width, num_procs);
-    const int anomalous_num_rows = num_rows_per_proc + (width % num_procs);
-    const int anomalous_proc = num_procs - 1;
-    int num_elements_per_proc = num_rows_per_proc * width;
-    const int anomalous_num_elements = anomalous_num_rows * width;
-    if (proc_rank == anomalous_proc)
-        num_elements_per_proc = anomalous_num_elements;
-    const int mat_size = width * width;
-
-    double *send_buffer = NULL;
-    double *recv_buffer = NULL;
-    double *M_2tr = (double *)malloc(mat_size * sizeof(double));
-    check_mem(M_2tr);
+    int num_rows_per_proc = width / num_procs;
+    const int unbalanced_proc = num_procs - 1;
     if (proc_rank == 0)
     {
         check_mem(M_1); check_mem(M_2); check_mem(P);
-        for (int i = 0; i < mat_size; i++)
-        {
-            const int col = i%width;
-            const int row = i/width;
-            M_2tr[col*row + width] = M_2[row*col + width];
-        }
+    }
+    check(num_rows_per_proc > 0, "Poorly balanced problem: (%d rows, %d processes)", width, num_procs);
+    int num_elements_per_proc = num_rows_per_proc * width;
+    const int mat_size = width * width;
+    // not worrying about load balancing for the time being
+    const int unbalanced_num_elements = mat_size - ((num_elements_per_proc) * (num_procs-1));
+    const int unbalanced_num_rows = unbalanced_num_elements/width;
+    
+
+    
+    // all processes will need a copy of process 2
+    if (proc_rank != 0)
+    {
+        M_2 = (double *)malloc(mat_size * sizeof(double));
+        check_mem(M_2);
     }
 
-    // broadcasting the transposed matrix from process 0
-    MPI_Bcast(M_2tr, mat_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Bcast(M_2, mat_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+    // send_counts for scatter_v
+    int *send_counts = (int *)malloc(num_procs * sizeof(int));
+    int *displacements = (int *)malloc(num_procs * sizeof(int));
+    displacements[0] = 0;
 
-    if (proc_rank == 0)
+    for (int i = 0; i < num_procs-1; i++)
     {
-        int skip = 0;
-        for (int proc = 1; proc < num_procs-1; proc++)
-        {
-            MPI_Send(M_1 + skip, num_elements_per_proc,
-                    MPI_DOUBLE, proc, 0, MPI_COMM_WORLD);
-            skip += num_elements_per_proc;
-        }
-
-        MPI_Send(M_1 + skip, anomalous_num_elements,
-                MPI_DOUBLE, anomalous_proc, 0, MPI_COMM_WORLD);
-
-        skip = 0;
-        for (int proc = 1; proc < num_procs-1; proc++)
-        {
-            MPI_Recv(P + skip, num_elements_per_proc, MPI_DOUBLE,
-                    proc, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            skip += num_elements_per_proc;
-        }
-
-        MPI_Recv(P + skip, anomalous_num_elements, MPI_DOUBLE,
-                anomalous_proc, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        send_counts[i] = num_elements_per_proc;
+        displacements[i+1] = displacements[i] + num_elements_per_proc;
     }
-    else 
+    // last process gets more elements than the others
+    send_counts[num_procs-1] = unbalanced_num_elements;
+
+    if (proc_rank == num_procs - 1) 
     {
-        recv_buffer = (double *)malloc(num_elements_per_proc);
-        check_mem(recv_buffer);
-        send_buffer = (double *)malloc(num_elements_per_proc);
-        check_mem(send_buffer);
-        MPI_Recv(recv_buffer, num_elements_per_proc, MPI_DOUBLE,
-                0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for (int i = 0 ; i < num_elements_per_proc; i++)
+        num_elements_per_proc = unbalanced_num_elements;
+        num_rows_per_proc = unbalanced_num_rows;
+    }
+
+    double *recv_buf = NULL, *send_buf = NULL;  
+    recv_buf = (double *)malloc(num_elements_per_proc * sizeof(double));
+    check_mem(recv_buf);
+    send_buf = (double *)malloc(num_elements_per_proc * sizeof(double));
+    check_mem(recv_buf);
+     
+
+    // scattering M_1
+    MPI_Scatterv(M_1, send_counts, displacements, MPI_DOUBLE,
+            recv_buf, num_elements_per_proc, MPI_DOUBLE, 
+            0, MPI_COMM_WORLD);
+
+
+    for (int row = 0; row < num_rows_per_proc; row++)
+    {
+        for (int col = 0; col < width; col++)
         {
+            int index = row*width + col;
             double sum = 0.0l;
-            const int row = i/width;
-            const int col = i%width;
-            for (int j = 0; j < width; j++)
+            for (int i = 0; i < width; i++)
             {
-                sum += recv_buffer[row*width + j] * M_2tr[col*width + j];
+                sum += recv_buf[row * width + i] * M_2[i * width + col];
+                if (proc_rank == unbalanced_proc)
+                {
+                    debug("%lf", sum);
+                }
+
             }
-            send_buffer[i] = sum;
+            send_buf[index] = sum;
         }
-        MPI_Send(send_buffer, num_elements_per_proc, MPI_DOUBLE,
-                0, 1, MPI_COMM_WORLD);
-        free(recv_buffer);
-        free(send_buffer);
     }
 
-    free(M_2tr);
+    MPI_Gatherv(send_buf, num_elements_per_proc, MPI_DOUBLE,
+            P, send_counts, displacements, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+
+    free(send_buf);
+    free(recv_buf);
+
+    
+    if (proc_rank != 0)
+    {
+        free(M_2);
+    }
+    MPI_Finalize();
+
     return EXIT_SUCCESS;
 error:
-    if (M_2tr) free(M_2tr); 
-    if (recv_buffer) free(recv_buffer);
-    if (send_buffer) free(send_buffer);
+    if (proc_rank != 0 && M_2)
+        free(M_2);
     return EXIT_FAILURE;
 }
 
 int
-matMulSquare_baseline_mpi(const double *M_1, double *M_2, double *P,
-        int width, int proc_rank, int num_procs)
+matMulSquare_transpose_mpi(const double *M_1, double *M_2,
+        double *P, int width,
+        int proc_rank, int num_procs)
 {
-    const int num_rows_per_proc = width / num_procs;
-    check(num_rows_per_proc > 0, 
-            "Poorly balanced problem;\
-            width of matrices: %d, number of processes: %d",
-            width, num_procs);
-    const int anomalous_num_rows = num_rows_per_proc + (width % num_procs);
-    const int anomalous_proc = num_procs - 1;
-    int num_elements_per_proc = num_rows_per_proc * width;
-    const int anomalous_num_elements = anomalous_num_rows * width;
-    if (proc_rank == anomalous_proc)
-        num_elements_per_proc = anomalous_num_elements;
-    const int mat_size = width * width;
-
-    double *send_buffer = NULL;
-    double *recv_buffer = NULL;
-    if (proc_rank == 0)
-    {
-        check_mem(M_1); check_mem(M_2); check_mem(P);
-        
-    }
-
-    // broadcasting the transposed matrix from process 0
-    MPI_Bcast(M_2, mat_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-
-    if (proc_rank == 0)
-    {
-        int skip = 0;
-        for (int proc = 1; proc < num_procs-1; proc++)
-        {
-            MPI_Send(M_1 + skip, num_elements_per_proc,
-                    MPI_DOUBLE, proc, 0, MPI_COMM_WORLD);
-            skip += num_elements_per_proc;
-        }
-
-        MPI_Send(M_1 + skip, anomalous_num_elements,
-                MPI_DOUBLE, anomalous_proc, 0, MPI_COMM_WORLD);
-
-        skip = 0;
-        for (int proc = 1; proc < num_procs-1; proc++)
-        {
-            MPI_Recv(P + skip, num_elements_per_proc, MPI_DOUBLE,
-                    proc, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            skip += num_elements_per_proc;
-        }
-
-        MPI_Recv(P + skip, anomalous_num_elements, MPI_DOUBLE,
-                anomalous_proc, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    else 
-    {
-        recv_buffer = (double *)malloc(num_elements_per_proc);
-        check_mem(recv_buffer);
-        send_buffer = (double *)malloc(num_elements_per_proc);
-        check_mem(send_buffer);
-        MPI_Recv(recv_buffer, num_elements_per_proc, MPI_DOUBLE,
-                0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for (int i = 0 ; i < num_elements_per_proc; i++)
-        {
-            double sum = 0.0l;
-            const int row = i/width;
-            const int col = i%width;
-            for (int j = 0; j < width; j++)
-            {
-                sum += recv_buffer[row*width + j] * M_2[j*width + col];
-            }
-            send_buffer[i] = sum;
-        }
-        MPI_Send(send_buffer, num_elements_per_proc, MPI_DOUBLE,
-                0, 1, MPI_COMM_WORLD);
-        free(recv_buffer);
-        free(send_buffer);
-    }
-
-    return EXIT_SUCCESS;
-error:
-    if (recv_buffer) free(recv_buffer);
-    if (send_buffer) free(send_buffer);
-    return EXIT_FAILURE;
+    return NOT_IMPLEMENTED;
 }
 
-
-int 
-matMulSquare_pretranspose_mpi(const double *M_1, double *M_2, double *P,
-        int width, int proc_rank, int num_procs)
+int
+matMulSquare_pretranspose_mpi(const double *M_1, double *M_2,
+        double *P, int width,
+        int proc_rank, int num_procs)
 {
-    const int num_rows_per_proc = width / num_procs;
-    check(num_rows_per_proc > 0, 
-            "Poorly balanced problem;\
-            width of matrices: %d, number of processes: %d",
-            width, num_procs);
-    const int anomalous_num_rows = num_rows_per_proc + (width % num_procs);
-    const int anomalous_proc = num_procs - 1;
-    int num_elements_per_proc = num_rows_per_proc * width;
-    const int anomalous_num_elements = anomalous_num_rows * width;
-    if (proc_rank == anomalous_proc)
-        num_elements_per_proc = anomalous_num_elements;
-    const int mat_size = width * width;
-
-    double *send_buffer = NULL;
-    double *recv_buffer = NULL;
-    if (proc_rank == 0)
-    {
-        check_mem(M_1); check_mem(M_2); check_mem(P);
-        
-    }
-
-    // broadcasting the transposed matrix from process 0
-    MPI_Bcast(M_2, mat_size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-
-    if (proc_rank == 0)
-    {
-        int skip = 0;
-        for (int proc = 1; proc < num_procs-1; proc++)
-        {
-            MPI_Send(M_1 + skip, num_elements_per_proc,
-                    MPI_DOUBLE, proc, 0, MPI_COMM_WORLD);
-            skip += num_elements_per_proc;
-        }
-
-        MPI_Send(M_1 + skip, anomalous_num_elements,
-                MPI_DOUBLE, anomalous_proc, 0, MPI_COMM_WORLD);
-
-        skip = 0;
-        for (int proc = 1; proc < num_procs-1; proc++)
-        {
-            MPI_Recv(P + skip, num_elements_per_proc, MPI_DOUBLE,
-                    proc, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            skip += num_elements_per_proc;
-        }
-
-        MPI_Recv(P + skip, anomalous_num_elements, MPI_DOUBLE,
-                anomalous_proc, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    }
-    else 
-    {
-        recv_buffer = (double *)malloc(num_elements_per_proc);
-        check_mem(recv_buffer);
-        send_buffer = (double *)malloc(num_elements_per_proc);
-        check_mem(send_buffer);
-        MPI_Recv(recv_buffer, num_elements_per_proc, MPI_DOUBLE,
-                0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        for (int i = 0 ; i < num_elements_per_proc; i++)
-        {
-            double sum = 0.0l;
-            const int row = i/width;
-            const int col = i%width;
-            for (int j = 0; j < width; j++)
-            {
-                sum += recv_buffer[row*width + j] * M_2[col*width + j];
-            }
-            send_buffer[i] = sum;
-        }
-        MPI_Send(send_buffer, num_elements_per_proc, MPI_DOUBLE,
-                0, 1, MPI_COMM_WORLD);
-        free(recv_buffer);
-        free(send_buffer);
-    }
-
-    return EXIT_SUCCESS;
-error:
-    if (recv_buffer) free(recv_buffer);
-    if (send_buffer) free(send_buffer);
-    return EXIT_FAILURE;
+    return NOT_IMPLEMENTED;
 }
 
---
+++ src/test_mpi.c
@@ -0,0 +1,96 @@
+#include "dbg.h"
+#include "impl_mpi.h"
+
+#include <inttypes.h>
+#include <mpi.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const double threshold = 0.01;
+
+
+int main(int argc, char *argv[])
+{
+    double *m1 = NULL, *m2 = NULL, *p = NULL;
+    int width, proc_rank, num_procs;
+    int mpi_err = MPI_Init(&argc, &argv);
+    check(!mpi_err, "MPI failed to initialize.");
+
+    mpi_err = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+    check(!mpi_err, "MPI_Comm_size failed");
+    mpi_err = MPI_Comm_rank(MPI_COMM_WORLD, &proc_rank);
+    check(!mpi_err, "MPI_Comm_rank failed");
+
+    if (proc_rank == 0)
+    {
+        int scan_count = scanf("%d", &width);
+        check(scan_count != EOF, "Unexpected EOF");
+        check(scan_count > 0, "Nothing scanned");
+        int mat_size = width * width;
+
+        m1 = (double *)malloc(mat_size * sizeof(double));
+        m2 = (double *)malloc(mat_size * sizeof(double));
+        p = (double *)malloc(mat_size * sizeof(double));
+        for (int i = 0; i < mat_size; i++)
+        {
+            scan_count = scanf("%lf", m1 + i);
+            check(scan_count != EOF, "Unexpected EOF");
+            check(scan_count > 0, "Nothing scanned");
+        }
+        for (int i = 0; i < mat_size; i++)
+        {
+            scan_count = scanf("%lf", m2 + i);
+            check(scan_count != EOF, "Unexpected EOF");
+            check(scan_count > 0, "Nothing scanned");
+        }
+    }
+
+    MPI_Bcast(&width, 1, MPI_INTEGER, 0, MPI_COMM_WORLD);
+    int mat_size = width * width;
+    int my_err;
+
+#ifdef TRANSPOSE
+    my_err = matMulSquare_transpose_mpi(m1, m2, p, width, proc_rank, num_procs);
+#endif
+#ifdef BASELINE
+    my_err = matMulSquare_baseline_mpi(m1, m2, p, width, proc_rank, num_procs);
+    check(!my_err, "Process %d: Something went wrong in matMul", proc_rank);
+#endif
+#ifdef PRETRANSPOSE
+    my_err = matMulSquare_pretranspose_mpi(m1, m2, p, width, proc_rank, num_procs);
+#endif
+    if (proc_rank == 0) {
+        for (int i = 0; i < mat_size; i++)
+        {
+            double elmt;
+            scanf("%lf", &elmt);
+            double residue = elmt - p[i];
+            if (residue >= threshold)
+            {
+                log_warn("At index %d:\
+                    expected %lf, eound %lf", i, elmt, p[i]);
+
+            }
+            else 
+            {
+                log_info("%d: Ye", i);
+            }
+            //(residue <= threshold, "At index %d:\
+                    expected %lf, eound %lf", i, elmt, p[i]);
+        }
+
+        free(m1);
+        free(m2);
+        free(p);
+    }
+    debug_mpi(proc_rank, "Exit success");
+    return EXIT_SUCCESS; 
+error:
+    if (m1) free(m1);
+    if (m2) free(m2);
+    if (p) free(p);
+    return EXIT_FAILURE;
+}
+
+// mpi_bcast
